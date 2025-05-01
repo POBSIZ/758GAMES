064edf72ed8e87c6b375e191365b149826115fab
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2025-05-01 18:09:12 +0900

    update

--- configuration/webpack.prod.config.js
+++ configuration/webpack.prod.config.js
@@ -1,12 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { merge } = require('webpack-merge');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const { merge } = require("webpack-merge");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
-const webpackConfiguration = require('../webpack.config');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const webpackConfiguration = require("../webpack.config");
 
 module.exports = merge(webpackConfiguration, {
-  mode: 'production',
+  mode: "production",
 
   /* Manage source maps generation process. Refer to https://webpack.js.org/configuration/devtool/#production */
   devtool: false,
@@ -29,5 +32,13 @@ module.exports = merge(webpackConfiguration, {
   },
 
   /* Additional plugins configuration */
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public/_redirects", to: "" }],
+    }),
+  ],
 });
